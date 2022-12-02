511d63fe59fa911a87ff805e41bde09ce936efbd
SashaJarvi <alexandrermakov2@gmail.com>
2022-12-02 18:48:25 +0400

    added questions and answered questions amounts output in categories screen

--- src/js/stores/categories.js
+++ src/js/stores/categories.js
@@ -1,19 +1,36 @@
 import {computed, ref} from 'vue';
 import {defineStore} from 'pinia';
 import api from '@/js/api';
+import {useAuthStore} from '@/js/stores/auth';
 import {useCategoryAnswerStore} from '@/js/stores/category-answer';
 
 export const useCategoryStore = defineStore('category', () => {
   const categories = ref([]);
   const category = ref(null);
 
+  const authStore = useAuthStore();
   const categoryAnswersStore = useCategoryAnswerStore();
 
-  const categoriesData = computed(() => categories.value);
+  const categoriesData = computed(() => {
+    return categories.value.map(c => {
+      const questionsArr = c.attributes.questions.data
+      const userAnswersArr = questionsArr.map(
+        q => q.attributes.user_answers.data.filter(d => d.attributes.users_permissions_user.data?.id === authStore.userData.id)
+      ).flat();
+
+      return {
+        ...c,
+        questions_amount: questionsArr.length,
+        user_answers_amount: userAnswersArr.length
+      }
+    })
+  });
   const categoryData = computed(() => category.value);
 
   const getCategories = async () => {
-    api.get('categories').then(res => res.json()).then(data => {
+    api.get('categories?populate[0]=questions&populate[1]=questions.user_answers&populate[2]=questions.user_answers.users_permissions_user')
+      .then(res => res.json())
+      .then(data => {
       categories.value = data.data;
     })
   };
