8409f897fc797189599a3e9713dd594748b7bf4e
SashaJarvi <alexandrermakov2@gmail.com>
2022-12-06 20:03:19 +0400

    added top-list component's layout

---
+++ src/components/top-list.vue
@@ -0,0 +1,29 @@
+<template>
+  <f7-block class="top-list">
+    <f7-row no-gap>
+      <f7-col>Nickname</f7-col>
+      <f7-col>1000</f7-col>
+    </f7-row>
+    <f7-row no-gap>
+      <f7-col>Nickname</f7-col>
+      <f7-col>1000</f7-col>
+    </f7-row>
+    <f7-row no-gap>
+      <f7-col>Nickname</f7-col>
+      <f7-col>1000</f7-col>
+    </f7-row>
+  </f7-block>
+</template>
+
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
+
+<style scoped>
+@import "@/assets/scss/components/top-list.scss";
+</style>
--- src/pages/top-lists.vue
+++ src/pages/top-lists.vue
@@ -1,11 +1,56 @@
 <template>
   <f7-page name="top-lists">
-    <f7-navbar title="Top Lists" back-link="Back"></f7-navbar>
-    <f7-block-title>Top Lists</f7-block-title>
+    <f7-navbar title="Top Lists" back-link="Back" />
+
+    <f7-block v-if="!shouldUpdateInfo">
+      <p>Please update your information to see your position</p>
+    </f7-block>
+
+    <f7-list accordion-list>
+      <f7-list-item accordion-item title="World">
+        <f7-accordion-content>
+          <top-list />
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="Country">
+        <f7-accordion-content>
+          <top-list />
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="City">
+        <f7-accordion-content>
+          <top-list />
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="School/University/College">
+        <f7-accordion-content>
+          <top-list />
+        </f7-accordion-content>
+      </f7-list-item>
+      <f7-list-item accordion-item title="Class/course">
+        <f7-accordion-content>
+          <top-list />
+        </f7-accordion-content>
+      </f7-list-item>
+    </f7-list>
   </f7-page>
 </template>
 
 <script setup>
+import {computed} from 'vue';
+import {storeToRefs} from 'pinia';
+import {useAuthStore} from '@/js/stores/auth';
+import TopList from '@/components/top-list.vue'
+
+const authStore = useAuthStore();
+const {user} = storeToRefs(authStore);
+
+const shouldUpdateInfo = computed(() =>
+  user.value.country
+  && user.value.city
+  && user.value.institution
+  && user.value.course
+)
 
 </script>
 
