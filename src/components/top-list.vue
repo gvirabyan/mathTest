e1376e801657950be813d58a6aeb9fb82b8ec540
SashaJarvi <alexandrermakov2@gmail.com>
2022-12-08 16:34:09 +0400

    set up getting data for top lists, added getting data method, added search inputs

--- src/components/top-list.vue
+++ src/components/top-list.vue
@@ -1,26 +1,43 @@
 <template>
   <f7-block class="top-list">
-    <f7-row no-gap>
-      <f7-col>Nickname</f7-col>
-      <f7-col>1000</f7-col>
+    <slot name="updateInfo" />
+    <slot name="searchInput" />
+
+    <f7-row v-if="isLoading">
+      <f7-col>
+        <f7-skeleton-block effect="wave" />
+      </f7-col>
+      <f7-col>
+        <f7-skeleton-block effect="wave" />
+      </f7-col>
     </f7-row>
-    <f7-row no-gap>
-      <f7-col>Nickname</f7-col>
-      <f7-col>1000</f7-col>
+
+    <f7-row v-else-if="!items.length">
+      <f7-col>There are no results yet</f7-col>
     </f7-row>
-    <f7-row no-gap>
-      <f7-col>Nickname</f7-col>
-      <f7-col>1000</f7-col>
+
+    <f7-row
+      v-else
+      v-for="({nickname, username, points}, index) in items"
+      :key="`top-list-row_${index + 1}`"
+      no-gap
+    >
+      <f7-col>{{ nickname || username }}</f7-col>
+      <f7-col class="text-right">{{ points }}</f7-col>
     </f7-row>
   </f7-block>
 </template>
 
 <script setup>
 defineProps({
+  isLoading: {
+    type: Boolean,
+    default: false
+  },
   items: {
     type: Array,
     default: () => []
-  }
+  },
 })
 </script>
 
