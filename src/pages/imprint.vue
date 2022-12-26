c06c80b4153d2ba7f215a9f941566007d5760557
SashaJarvi <alexandrermakov2@gmail.com>
2022-12-26 14:06:51 +0400

    created settings page and its child pages

---
+++ src/pages/imprint.vue
@@ -0,0 +1,13 @@
+<template>
+  <f7-page name="imprint">
+    <f7-navbar title="Imprint" back-link="Back" />
+    <f7-block-title>Imprint</f7-block-title>
+  </f7-page>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/terms.vue
@@ -0,0 +1,13 @@
+<template>
+  <f7-page name="terms">
+    <f7-navbar title="Terms of services" back-link="Back" />
+    <f7-block-title>Terms of services</f7-block-title>
+  </f7-page>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
