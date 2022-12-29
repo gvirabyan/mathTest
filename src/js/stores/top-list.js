f2d4120c4333d04512336c04ac6619e812a086c8
SashaJarvi <alexandrermakov2@gmail.com>
2022-12-29 11:30:08 +0400

    fixed getting of results by institution and course

--- src/js/stores/top-list.js
+++ src/js/stores/top-list.js
@@ -1,15 +1,24 @@
 import { ref } from 'vue';
 import { defineStore } from 'pinia';
+import {useAuthStore} from '@/js/stores/auth';
 import api from '@/js/api';
 
 export const useTopList = defineStore('topList', () => {
+  const {user} = useAuthStore();
+
   const topList = ref([]);
   const topListMeta = ref(null);
 
   const getTopList = (filter = '', value = '', page = 1) => {
-    const url = filter !== 'world'
-      ? `get-points?filters[${filter}]=${value}&sort=points:desc&pagination[page]=${page}&pagination[pageSize]=20`
-      : `get-points?sort=points:desc&pagination[page]=${page}&pagination[pageSize]=20`
+    let url;
+
+    if (filter === 'world') {
+      url = `get-points?sort=points:desc&pagination[page]=${page}&pagination[pageSize]=20`
+    } else if (filter === 'course') {
+      url = `get-points?filters[institution]=${user.institution.place_id}&filters[${filter}]=${value}&sort=points:desc&pagination[page]=${page}&pagination[pageSize]=20`
+    } else {
+      url = `get-points?filters[${filter}]=${value}&sort=points:desc&pagination[page]=${page}&pagination[pageSize]=20`
+    }
 
     return api.get(url)
       .then(res => res.json())
