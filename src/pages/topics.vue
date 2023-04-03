2de9fe7d66244c55fecfe7a63411d1558b04ddf6
SashaJarvi <alexandrermakov2@gmail.com>
2023-04-03 14:24:02 +0400

    added categories finding by search string logics, finished styling

--- src/pages/topics.vue
+++ src/pages/topics.vue
@@ -53,18 +53,43 @@
           <img class="input-icon" src="@/assets/icons/arrow-right.svg" alt="" />
         </div>
 
-        <div class="keywords">
-          <div v-for="(keyword, index) in keywords" :key="`keyword_${index + 1}`" class="keyword">
-            {{ keyword }}
-          </div>
+        <div v-if="!searchStr" class="keywords">
+          <f7-button
+            v-for="({ name, active }, index) in keywords"
+            :key="`keyword_${index + 1}`"
+            class="keyword"
+            :class="{ active: active }"
+            @click="selectKeyword(index)"
+          >
+            {{ name }}
+          </f7-button>
         </div>
+
+        <f7-list v-if="searchedCategoriesData?.length" no-hairlines-md>
+          <f7-list-item
+            v-for="category in searchedCategoriesData"
+            :key="category.id"
+            @click="goToQuestions(category.id)"
+          >
+            <template #title>
+              <span class="classes-subtitle"
+                >{{ category.attributes.category_class.data.attributes.name }} classes</span
+              >
+              <text-clamp :text="category.attributes.name" :max-lines="2" :max-width="280" ellipsis="" />
+            </template>
+
+            <template #after>
+              <p>{{ getAfterText(category) }}</p>
+            </template>
+          </f7-list-item>
+        </f7-list>
       </f7-page>
     </f7-popup>
   </f7-page>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import { storeToRefs } from "pinia";
 import TextClamp from "vue3-text-clamp";
 import { useCategoryStore } from "@/js/stores/categories";
@@ -72,23 +97,49 @@ import { useCategoryClassesStore } from "@/js/stores/category-classes";
 import delay from "@/js/helpers/delay";
 import TopBar from "@/components/topbar.vue";
 import BottomMenu from "@/components/bottom-menu.vue";
+import useDebouncedRef from "@/js/composables/use-debounced-ref";
 
-defineProps({
+const props = defineProps({
+  f7router: Object,
   f7route: Object,
 });
 
 const categoriesStore = useCategoryStore();
 const categoriesClassesStore = useCategoryClassesStore();
-const { categoriesData } = storeToRefs(categoriesStore);
+const { categoriesData, searchedCategoriesData } = storeToRefs(categoriesStore);
 const { categoryClasses } = storeToRefs(categoriesClassesStore);
-const { getCategoriesByCategoryClass } = categoriesStore;
+const { getCategories, getCategoriesByCategoryClass, clearSearchedCategories } = categoriesStore;
 const { getCategoryClasses } = categoriesClassesStore;
-
-const keywords = ["Razionale zahlen", "Multipliziren", "Kommazahlen", "Prozent", "Rationale", "Dividieren"];
 
 const isLoading = ref(false);
 const isSearchPopup = ref(false);
-const searchStr = ref("");
+const searchStr = useDebouncedRef("");
+const keywords = ref([
+  {
+    name: "Rationale zahlen",
+    active: false,
+  },
+  {
+    name: "Multiplizieren",
+    active: false,
+  },
+  {
+    name: "Kommazahlen",
+    active: false,
+  },
+  {
+    name: "Prozent",
+    active: false,
+  },
+  {
+    name: "Rationale",
+    active: false,
+  },
+  {
+    name: "Dividieren",
+    active: false,
+  },
+]);
 
 const classesTabs = computed(() =>
   categoryClasses.value.map(c => ({ id: c.id, name: `${c.attributes.name} classes` })),
@@ -117,8 +168,29 @@ const getCategoriesByClass = async id => {
 };
 
 const togglePopup = () => {
+  if (isSearchPopup.value) {
+    searchStr.value = "";
+    keywords.value.forEach(k => (k.active = false));
+    clearSearchedCategories();
+  }
+
   isSearchPopup.value = !isSearchPopup.value;
 };
+
+const selectKeyword = index => {
+  keywords.value = keywords.value.map((k, i) => ({ ...k, active: i === index }));
+  searchStr.value = keywords.value[index].name;
+};
+
+const goToQuestions = categoryId => {
+  togglePopup();
+  props.f7router.navigate(`/categories/${categoryId}/questions/`);
+};
+
+watch(searchStr, async value => {
+  clearSearchedCategories();
+  value && (await getCategories(value));
+});
 </script>
 
 <style lang="scss">
