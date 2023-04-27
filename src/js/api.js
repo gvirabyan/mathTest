8661b0384c2c979ff943bd223ad181827312f9d6
seyran2002 <97274895+seyran2002@users.noreply.github.com>
2023-04-27 11:16:12 +0400

    complete request canceling

--- src/js/api.js
+++ src/js/api.js
@@ -7,11 +7,22 @@ const defaultOptions = () => {
   if (localStorage.getItem("token")) {
     options.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   }
-
   return options;
 };
 
-const get = url => fetch(`${import.meta.env.VITE_API_URL}${url}`, defaultOptions());
+let prevUrl = '';
+const get = async url => {
+  if (prevUrl && prevUrl.split('&')[0] === url.split('&')[0]) {
+    if (window.controller) {
+      window.controller.abort()
+    }
+    window.controller = new AbortController();
+    window.signal = window.controller.signal;
+  }
+  prevUrl = url;
+  const request = await fetch(`${import.meta.env.VITE_API_URL}${url}`, { signal: window.signal, ...defaultOptions() });
+  return request;
+};
 
 const post = (url, body) =>
   fetch(`${import.meta.env.VITE_API_URL}${url}`, {
@@ -32,7 +43,6 @@ const remove = url =>
     ...defaultOptions(),
     method: "DELETE",
   });
-
 export default {
   get,
   post,
