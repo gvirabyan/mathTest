ce43495c7fda3c7959b878b7788d8a55a0e795c6
SashaJarvi <alexandrermakov2@gmail.com>
2023-05-18 17:51:57 +0400

    removed editParentEmail() action

--- src/js/stores/parents-emails.js
+++ src/js/stores/parents-emails.js
@@ -25,47 +25,39 @@ export const useParentsEmailsStore = defineStore("parents-emails", () => {
   };
 
   const saveParentsEmails = emails => {
-    return api.post(`saveMultipleParentsEmails`, { data: { emails } }).then(data => {
-      if (!data.error) {
-        parentsEmails.value = [...parentsEmails.value, ...data?.data];
+    return api
+      .post(`saveMultipleParentsEmails`, { data: { emails } })
+      .then(res => res.json())
+      .then(data => {
+        if (!data.error) {
+          parentsEmails.value = [...parentsEmails.value, ...data?.data];
 
-        return { status: "success" };
-      } else {
-        return { status: "error", message: data.error?.message };
-      }
-    });
-  };
-
-  const editParentEmail = (id, email) => {
-    return api.put(`parents-emails/${id}?populate[0]=users_permissions_users`, { data: { email } }).then(data => {
-      if (!data.error) {
-        const updatedEmail = { id: data?.data.id, ...data?.data.attributes };
-        parentsEmails.value = parentsEmails.value.map(e => (e.id === updatedEmail.id ? updatedEmail : e));
-
-        return { status: "success" };
-      } else {
-        return { status: "error", message: data.error?.message };
-      }
-    });
+          return { status: "success" };
+        } else {
+          return { status: "error", message: data.error?.message };
+        }
+      });
   };
 
   const removeParentEmail = id => {
-    return api.remove(`parents-emails/${id}`).then(data => {
-      if (!data.error) {
-        parentsEmails.value = parentsEmails.value.filter(e => e.id !== id);
+    return api
+      .remove(`parents-emails/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        if (!data.error) {
+          parentsEmails.value = parentsEmails.value.filter(e => e.id !== id);
 
-        return { status: "success" };
-      } else {
-        return { status: "error", message: data.error?.message };
-      }
-    });
+          return { status: "success" };
+        } else {
+          return { status: "error", message: data.error?.message };
+        }
+      });
   };
 
   return {
     parentsEmails,
     getParentsEmails,
     saveParentsEmails,
-    editParentEmail,
     removeParentEmail,
   };
 });
