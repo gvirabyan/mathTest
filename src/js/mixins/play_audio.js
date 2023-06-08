5a81612b1fb3ef46f8565b57b86b1d7dcd77de93
seyran2002 <97274895+seyran2002@users.noreply.github.com>
2023-06-08 14:19:43 +0400

    check error

--- src/js/mixins/play_audio.js
+++ src/js/mixins/play_audio.js
@@ -5,7 +5,15 @@ const playAudioMixin = {
         `assets/audios/${name}.${["lose", "skipped", "notification"].includes(name) ? "mp3" : "wav"}`,
       );
       console.log(audio, 111);
-      audio.play();
+      audio
+        .play()
+        .then(function () {
+          // Automatic playback started!
+        })
+        .catch(function (error) {
+          // Automatic playback failed.
+          // Show a UI element to let the user manually start playback.
+        });
     };
     return {
       playAudio,
