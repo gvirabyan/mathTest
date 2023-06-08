59d87c5341e0761fcd113d8bcfe7f11e013339ac
seyran2002 <97274895+seyran2002@users.noreply.github.com>
2023-06-08 15:19:18 +0400

    finished task

--- src/js/mixins/play_audio.js
+++ src/js/mixins/play_audio.js
@@ -4,17 +4,7 @@ const playAudioMixin = {
       const audio = new Audio();
       audio.src = `audios/${name}.${["lose", "skipped", "notification"].includes(name) ? "mp3" : "wav"}`;
       audio.load();
-      audio
-        .play()
-        .then(function (r) {
-          // Automatic playback started!
-          console.log(r, 11);
-        })
-        .catch(function (error) {
-          console.log(error, 14);
-          // Automatic playback failed.
-          // Show a UI element to let the user manually start playback.
-        });
+      audio.play();
     };
     return {
       playAudio,
