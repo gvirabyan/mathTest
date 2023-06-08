6ab50df70bd6b24c534e183032bde8a200c85e57
seyran2002 <97274895+seyran2002@users.noreply.github.com>
2023-06-08 14:16:06 +0400

    check error

--- src/js/mixins/play_audio.js
+++ src/js/mixins/play_audio.js
@@ -4,6 +4,7 @@ const playAudioMixin = {
       const audio = new Audio(
         `assets/audios/${name}.${["lose", "skipped", "notification"].includes(name) ? "mp3" : "wav"}`,
       );
+      console.log(audio, 111);
       audio.play();
     };
     return {
