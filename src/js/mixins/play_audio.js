20c60b7e7add01cede94fe8b86a974c5a5c7ada2
seyran2002 <97274895+seyran2002@users.noreply.github.com>
2023-06-08 15:04:36 +0400

    change

--- src/js/mixins/play_audio.js
+++ src/js/mixins/play_audio.js
@@ -2,17 +2,16 @@ const playAudioMixin = {
   setup() {
     const playAudio = name => {
       const audio = new Audio();
-      console.log(audio, 111);
-      audio.src = `assets/audios/${name}.${["lose", "skipped", "notification"].includes(name) ? "mp3" : "wav"}`;
+      audio.src = `audios/${name}.${["lose", "skipped", "notification"].includes(name) ? "mp3" : "wav"}`;
       audio.load();
       audio
         .play()
         .then(function (r) {
           // Automatic playback started!
-          console.log(r, 12);
+          console.log(r, 11);
         })
         .catch(function (error) {
-          console.log(error, 12);
+          console.log(error, 14);
           // Automatic playback failed.
           // Show a UI element to let the user manually start playback.
         });
