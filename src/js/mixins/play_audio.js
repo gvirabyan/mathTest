aead14cd56597524fb615c67f9dbb0ef5b52d835
seyran2002 <97274895+seyran2002@users.noreply.github.com>
2023-06-08 14:09:00 +0400

    async await is removed

--- src/js/mixins/play_audio.js
+++ src/js/mixins/play_audio.js
@@ -1,10 +1,10 @@
 const playAudioMixin = {
   setup() {
-    const playAudio = async name => {
+    const playAudio = name => {
       const audio = new Audio(
         `assets/audios/${name}.${["lose", "skipped", "notification"].includes(name) ? "mp3" : "wav"}`,
       );
-      return await audio.play().then(r => r);
+      audio.play();
     };
     return {
       playAudio,
